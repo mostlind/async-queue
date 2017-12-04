d4e37a92df4a128c633a4cefb94361ac47c4e2d0
Matt <mostlind@gmail.com>
2017-12-04 10:59:14 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const a = ({ queue, waiting }) => ({
+  enq: x => {
+    waiting.length > 0 ? waiting.shift()(x) : queue.unshift(x);
+    return;
+  },
+  deq: async () =>
+    queue.length > 0
+      ? queue.shift()
+      : new Promise((resolve, _reject) => {
+          waiting.unshift(resolve);
+        })
+});
+
+const create = () => a({ queue: [], waiting: [] });
+const from = arr => a({ queue: [...arr], waiting: [] });
+
+export default {
+  create,
+  from
+};
